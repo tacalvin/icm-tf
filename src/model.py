36d90c6a847a9a823b5c04230b88c3c7b63736c7
Calvin <tacalvin123@gmail.com>
2018-01-07 11:19:10 -0800

    Value Loss change

--- src/model.py
+++ src/model.py
@@ -74,7 +74,7 @@ class A3C_Network():
                 self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
 
                 #Loss functions
-                self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+                self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.policy - self.target_v))
                 self.entropy = - tf.reduce_sum(self.policy * tf.log(self.policy))
                 self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs) * self.advantages)
                 self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01
